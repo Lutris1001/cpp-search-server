167fefb423a836c9aedcabd5e263fc0928779e5b
Lutris1001 <Lutris1001@gmail.com>
2022-03-27 20:45:44 +0300

    Forth_sprint_done

---
+++ search-server/request_queue.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <deque>
+#include "request_queue.h"
+#include "document.h"
+
+using namespace std;
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+    StepForNewRequest();
+    vector<Document> result = search_server_.FindTopDocuments(raw_query, status);
+    StepAfterRequest(result);
+    return result;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    StepForNewRequest();
+    vector<Document> result = search_server_.FindTopDocuments(raw_query);
+    StepAfterRequest(result);
+    return result;
+}
+
+int RequestQueue::GetNoResultRequests() const {
+    return empty_requests_;
+}
+
+void RequestQueue::StepForNewRequest() {
+    ++now_time;
+    if (now_time > min_in_day_ || requests_.size() >= min_in_day_) {
+        if ((requests_.front()).empty_result) {
+            --empty_requests_;
+        }
+        requests_.pop_front();
+    }
+}
+    
+void RequestQueue::StepAfterRequest(const vector<Document>& result) {
+    bool empty_result = !!(result.empty());
+    if (empty_result) {
+        ++empty_requests_;
+    }
+    QueryResult request = {result, empty_result};
+    requests_.push_back(request);
+}
+    
+
---
+++ search-server/request_queue.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <deque>
+#include "document.h"
+#include "search_server.h"
+
+using namespace std;
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server)
+        : search_server_(search_server)
+    {
+    }
+
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+        StepForNewRequest();
+        vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        StepAfterRequest(result);
+        return result;
+    }
+
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status);
+
+    vector<Document> AddFindRequest(const string& raw_query);
+
+    int GetNoResultRequests() const;
+    
+private:
+    
+    struct QueryResult {
+        vector<Document> result;
+        bool empty_result;
+    };
+    
+    deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    int now_time = 0;
+    const SearchServer& search_server_;
+    int empty_requests_ = 0;
+    
+    void StepForNewRequest();
+    
+    void StepAfterRequest(const vector<Document>& result);
+};
+
