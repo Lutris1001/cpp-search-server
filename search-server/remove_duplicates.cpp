1bb802c0d923f15ccc8f17be4bef85fd1ef6584c
Lutris1001 <Lutris1001@gmail.com>
2022-04-18 16:57:50 +0300

    Fifth_sprint_with_duplicates_memory

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -5,20 +5,23 @@
 
 void RemoveDuplicates(SearchServer& search_server) {
     set<int> ids_to_remove;
-    set<set<string>> original_docs; // Набор оригинальных документов
+    static set<set<string>> original_docs; // Набор оригинальных документов
+    static auto last_id = search_server.begin(); // При повторном вызове функции
+    // если я все правильно сделал, то проверяться будут только новые документы
 
-    for (const int document_id : search_server) {
+    for (auto id = last_id; id != search_server.end(); ++id) {
         set<string> words;
-        for (const auto& [key, value] : search_server.GetWordFrequencies(document_id)) {
+        for (const auto& [key, value] : search_server.GetWordFrequencies(*id)) {
             words.insert(key);
         }
         // Если документ является дубликатом - то заносим его в список на удаление
         // при этом количество проверок внутри find растет только при увеличении количества
         // оригинальных документов и игнорируя дубликаты
         if (original_docs.find(words) != original_docs.end()) {
-            ids_to_remove.insert(document_id);
+            ids_to_remove.insert(*id);
         } else {
             original_docs.insert(words); // иначе записываем как оригинальный набор слов
+            last_id = id;
         }
     }
 
